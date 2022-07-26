dd2b3388ce6f15639e63156764f852962d0c0fbd
opyjo <opeyemiojoj@gmail.com>
2022-07-26 19:19:37 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import NasaImage from "./component/NasaImage";
+import NasaImageByDate from "./component/NasaImageByDate";
+const URL =
+  "https://api.nasa.gov/planetary/apod?api_key=Of6EBkfsyb6Ad0jZnHHDxRzxY9ghy0j0K5acT2q0";
 
 function App() {
   const [nasa, setNasa] = useState("");
+  const [date, setDate] = useState("");
+  const [showNasa, setShowNasa] = useState(false);
+  const [dateNasa, setDateNasa] = useState("");
 
-  const getImage = async () => {
-    const res = await axios.get(
-      "https://api.nasa.gov/planetary/apod?api_key=Of6EBkfsyb6Ad0jZnHHDxRzxY9ghy0j0K5acT2q0"
+  const dateHandler = (e) => {
+    setDate(e.target.value);
+  };
+
+  useEffect(() => {
+    const getImage = async () => {
+      const res = await axios.get(
+        `https://api.nasa.gov/planetary/apod?api_key=Of6EBkfsyb6Ad0jZnHHDxRzxY9ghy0j0K5acT2q0`
+      );
+      setNasa(res.data);
+      console.log(res.data);
+    };
+    getImage();
+  }, []);
+
+  const getDataByDate = async () => {
+    const response = await axios.get(
+      `https://api.nasa.gov/planetary/apod?api_key=Of6EBkfsyb6Ad0jZnHHDxRzxY9ghy0j0K5acT2q0&date=${date}`
     );
-    setNasa(res.data);
-    console.log(res.data);
+    setShowNasa(true);
+    setDateNasa(response.data);
   };
 
   return (
     <div>
-      <h1>Nasa Image</h1>
-      <button onClick={getImage}>Get Image</button>
+      <div>
+        <h1 className="header">nasa picture of the day</h1>
+      </div>
+
       <NasaImage nasa={nasa} />
+      {
+        <div className="container container2">
+          <h1 className="header">
+            Find the picture of the day for a previous date
+          </h1>
+          <label className="text" htmlFor="date">
+            Select Date
+          </label>
+          <input
+            className="text"
+            type="date"
+            onChange={dateHandler}
+            value={date}
+            id="date"
+          />
+          <button className="btn" onClick={getDataByDate}>
+            Get Image
+          </button>
+          {showNasa && <NasaImageByDate dateNasa={dateNasa} />}
+        </div>
+      }
     </div>
   );
 }
--- src/component/NasaImageByDate.js
+++ src/component/NasaImageByDate.js
@@ -1,19 +1,26 @@
 import React from "react";
 import "./NasaImage.css";
 
-const NasaImage = ({ nasa }) => {
+const NasaImage = ({ dateNasa }) => {
   return (
     <div>
-      <p className="text text1">{nasa.title}</p>
-      <p className="text">Image description/explanation:{nasa.explanation}</p>
-      {nasa.media_type === "image" ? (
+      <p className="text text1">{dateNasa.title}</p>
+      <p className="text">
+        Image description/explanation:{dateNasa.explanation}
+      </p>
+      {dateNasa.media_type === "image" ? (
         <div className="container">
-          <img height="500rem" width="500rem" src={nasa.url} alt={nasa.title} />
+          <img
+            height="500rem"
+            width="500rem"
+            src={dateNasa.url}
+            alt={dateNasa.title}
+          />
         </div>
       ) : (
         <div className="container">
           <iframe
-            src={nasa.url}
+            src={dateNasa.url}
             frameBorder="2"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
